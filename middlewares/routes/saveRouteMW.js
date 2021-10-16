71f15555786f1260babdcd74346d9908a7a45dce
Tschonti <feketesamu@gmail.com>
2021-10-16 21:10:07 +0200

    saveRoutMW has to handle get requests as well

--- middlewares/routes/saveRouteMW.js
+++ middlewares/routes/saveRouteMW.js
@@ -1,7 +1,8 @@
 //const requireOption = require('../requireOption');
 
 /**
- * Saves or updates a route with POST parameters. If res.locals.route exists, it's an update, otherwise it's creation.
+ * If method is GET, it does nothing.
+ * Otherwise it saves or updates a route with POST parameters. If res.locals.route exists, it's an update, otherwise it's creation.
  * If the params are okay, redirects to the newly created/updated route, otherwise stores errors in res.locals.errors.
  * @param {*} objectrepository
  * @returns
